913d8e6bec1f787a1de907911c218126e54763bb
Anasc13 <anasantacruz13@gmail.com>
2022-08-15 16:17:38 -0300

    home

--- Portfolio/src/app/componentes/home/home.component.ts
+++ Portfolio/src/app/componentes/home/home.component.ts
@@ -12,19 +12,55 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  acercalist: Acerca[] = [];
- 
-  educationList: Education[] = [];
- 
-  experienceList: Experience[] = [];
-
-  personaList: Persona[] = [];
+  acercas:Acerca[]= [];
+  acercalist:Acerca= { about:"" };
+  about:string=this.acercalist.about;
   
-  proyectosList: Proyectos[] = [];
-  
-  skillsList: Skills[] = [];
+  educations: Education[]= [];
+  educationList: Education= { school:"", title:"", img:"", career:"", score:"", start:"", end:""}
+  school=this.educationList.school;
+  title=this.educationList.title;
+  img=this.educationList.img;
+  career=this.educationList.career;
+  score=this.educationList.score;
+  start=this.educationList.start;
+  end=this.educationList.end;
 
-     
+  experiences: Experience[]= [];
+  experienceList: Experience= { position:"", company:"", img:"", mode:"", start:"", end:"", timeElapsed:"" }
+  position: string=this.experienceList.position;
+  company: string=this.experienceList.company;
+  expimg: string=this.experienceList.img;
+  mode: string=this.experienceList.mode;
+  expstart:string=this.experienceList.start;
+  expend:string=this.experienceList.end;
+  timeElapsed: string=this.experienceList.timeElapsed;
+
+  personas: Persona[]= [];
+  personaList: Persona= { nombre:"", apellido:"", mail:"", position:"", ubication:"", companyName:"", companyImg:"", companyUrl:"" }
+  nombre=this.personaList.nombre;
+  apellido=this.personaList.apellido;
+  mail=this.personaList.mail;
+  perposition=this.personaList.position;
+  ubication=this.personaList.ubication;
+  companyName=this.personaList.companyName;
+  companyImg=this.personaList.companyImg;
+  companyUrl=this.personaList.companyUrl;
+
+  proyectos: Proyectos[]= [];
+  proyectosList: Proyectos= { name:"", description:"", img:"", start:"", end:"", link:"" }
+  name:string=this.proyectosList.name;
+  description:string=this.proyectosList.description;
+  proyimg:string=this.proyectosList.img;
+  proystart: string=this.proyectosList.start;
+  proyend: string=this.proyectosList.end;
+  link:string=this.proyectosList.link;
+
+  skills: Skills[]= [];
+  skillsList: Skills= { name:"", percentage: ""}
+  skname: string=this.skillsList.name;
+  percentage: string=this.skillsList.percentage;
+       
 constructor(
   private logInService: LogInService,
   private ruta:Router, private personaService:PersonaService) 
@@ -32,24 +68,25 @@ constructor(
 
   ngOnInit(): void {
     this.personaService.getAcerca().subscribe((acerca)=>{
-      this.acercalist=acerca;
+      this.acercas=acerca;
     });
     this.personaService.getEducation().subscribe((education)=>{
-      this.educationList = education;
+      this.educations = education;
     });
     this.personaService.getExperience().subscribe((experience)=>{
-      this.experienceList = experience;
+      this.experiences = experience;
     });
     this.personaService.getAllPersonas().subscribe((persona)=>{
-    this.personaList=persona
+    this.personas=persona
     });
-    this.personaService.getProyectos().subscribe((Proyectos)=>{
-    this.proyectosList = Proyectos;
+    this.personaService.getProyectos().subscribe((proyecto)=>{
+    this.proyectos = proyecto;
     });
     this.personaService.getSkills().subscribe((skills)=>{
-    this.skillsList = skills;
+    this.skills = skills;
     });
   } 
+
 
   LogIn(event:Event){
 event.preventDefault;
